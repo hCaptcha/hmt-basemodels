820fe696598a003db9c39dafdbbe74056eef326a
Ville Toiviainen <villej.toiviainen@gmail.com>
2019-04-26 14:51:20 +0300

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="hmt-basemodels",
-    version="0.0.1",
+    version="0.0.2",
     author="HUMAN Protocol",
     description="Basemodels for manifest data used by hmt-escrow",
     url="https://github.com/hCaptcha/hmt-basemodels",
