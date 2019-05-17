fb657b5239331fbb69fbfef750ec6fe72f97c479
Ville Toiviainen <villej.toiviainen@gmail.com>
2019-05-17 18:39:18 +0300

    remove unused import

--- basemodels/__init__.py
+++ basemodels/__init__.py
@@ -1,7 +1,7 @@
 import uuid
 from schematics.models import Model, ValidationError
 from schematics.types import StringType, DecimalType, BooleanType, IntType, DictType, ListType, URLType, FloatType, \
-    UUIDType, ModelType, BooleanType, UnionType, MultiType
+    UUIDType, ModelType, BooleanType, UnionType
 
 
 class TaskData(Model):
