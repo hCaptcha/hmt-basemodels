5d8554930dc5e63eb23ba93547918353fd35b5d5
Evin Callahan <evin.callahan@gmail.com>
2019-10-07 18:28:39 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="hmt-basemodels",
-    version="0.0.11",
+    version="0.0.12",
     author="HUMAN Protocol",
     description="Common data models shared by various components of the Human Protocol stack",
     url="https://github.com/hCaptcha/hmt-basemodels",
