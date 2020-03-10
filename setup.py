b72a44a71bb9cad26f369f12d4f7b51c142adf68
Alex Newman <posix4e@gmail.com>
2020-03-09 19:46:56 -0700

    bump to v17

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="hmt-basemodels",
-    version="0.0.15",
+    version="0.0.17",
     author="HUMAN Protocol",
     description="Common data models shared by various components of the Human Protocol stack",
     url="https://github.com/hCaptcha/hmt-basemodels",
