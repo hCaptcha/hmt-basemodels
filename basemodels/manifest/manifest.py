e4756b391e9c6bd5bab997d92106537439c2ef04
rodriguez-facundo <frodriguez4600@gmail.com>
2022-07-04 14:44:51 -0300

    rename to overlap_threshold

--- basemodels/manifest/manifest.py
+++ basemodels/manifest/manifest.py
@@ -79,7 +79,7 @@ class RequestConfig(Model):
     minimum_selection_area_per_shape = IntType()
     multiple_choice_max_choices = IntType(default=1)
     multiple_choice_min_choices = IntType(default=1)
-    sensitivity = FloatType(required=False)
+    overlap_threshold = FloatType(required=False)
 
 class InternalConfig(Model):
     """ discarded from incoming manifests """
