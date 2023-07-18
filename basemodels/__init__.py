08a735e6d7bf750cd58d3d5789f8bfaa8f0d0bb1
Dickson Chibuzor <dickson.ek.chibuzor@gmail.com>
2023-07-18 15:12:20 +0100

    moving to pydantic

--- basemodels/__init__.py
+++ basemodels/__init__.py
@@ -1,4 +1,4 @@
-from .manifest import (
+from .pydantic.manifest import (
     validate_manifest_uris,
     validate_manifest_example_images,
     Manifest,
@@ -7,10 +7,12 @@ from .manifest import (
     TaskData,
     Webhook,
 )
-from .manifest.data import (
+from .pydantic.manifest.data import (
     validate_taskdata_entry,
     validate_groundtruth_entry,
     validate_requester_example_image,
     validate_requester_restricted_answer_set_uris,
 )
 from .via import ViaDataManifest
+
+# when I come back just test with this in yellow. Run different kind of jobs with this and see it works properly
