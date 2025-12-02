a1d9568287afb9100fd9edd3aa3f2f65a9e5dee1
Dickson Chibuzor <dickson.ek.chibuzor@gmail.com>
2025-12-02 11:34:55 +0100

    fix actions

--- basemodels/manifest/manifest.py
+++ basemodels/manifest/manifest.py
@@ -298,7 +298,7 @@ class Manifest(Model):
     webhook: Optional[Webhook] = None
 
     rejected_uri: Optional[AnyHttpUrl] = None
-    rejected_count: Optional[int] = None
+    rejected_count: Optional[int] = None  # check
 
     enable_skip_text: Optional[bool] = False
 
